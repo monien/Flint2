653b3381c9104c2b827f7f2f6e97bec377b22b29
Hartmut Monien <hmonien@uni-bonn.de>
2022-09-14 19:46:12 +0200

    cleanup.

--- csrc/fmpz/clear.c
+++ csrc/fmpz/clear.c
@@ -3,14 +3,9 @@
 #include <flint/flint.h>
 #include <flint/fmpz.h>
 
-<<<<<<< HEAD
 void p_fmpz_clear(fmpz_t x) {
 #ifdef DEBUG
   flint_fprintf(stderr, "p_fmpz_clear 0x%016p\n", x);
 #endif
-=======
-void fmpz_clear_(fmpz_t x) {
-  flint_fprintf(stderr, "fmpz_clear_ 0x%016p\n", x);
->>>>>>> 471624ab3067dcc8cfcb4e6b7f648e6cb6172ce2
   fmpz_clear(x);
 }
