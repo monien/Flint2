94b4b8dde9660e7a9349cc9ffb571b3d74f0305f
Hartmut Monien <hmonien@uni-bonn.de>
2023-02-24 09:01:18 +0100

    added groups perm.

--- src/Data/Number/Flint.hs
+++ src/Data/Number/Flint.hs
@@ -11,6 +11,7 @@ module Data.Number.Flint (
 , module Data.Number.Flint.Fmpq.Mat
 , module Data.Number.Flint.Fmpq.Vec
 , module Data.Number.Flint.Fmpq.Poly
+, module Data.Number.Flint.Groups.Perm
 , module Data.Number.Flint.Padic
 , module Data.Number.Flint.Padic.Poly
 , module Data.Number.Flint.Padic.Mat
@@ -31,6 +32,7 @@ import Data.Number.Flint.Fmpq
 import Data.Number.Flint.Fmpq.Mat
 import Data.Number.Flint.Fmpq.Vec
 import Data.Number.Flint.Fmpq.Poly
+import Data.Number.Flint.Groups.Perm
 import Data.Number.Flint.Padic
 import Data.Number.Flint.Padic.Poly
 import Data.Number.Flint.Padic.Mat
---
+++ src/Data/Number/Flint/Groups/Perm.hs
@@ -0,0 +1,13 @@
+{- | 
+   module      : Data.Number.Flint.MPoly
+   copyright   :  (c) 2019 Hartmut Monien
+   license     :  BSD-style (see LICENSE)
+   maintainer  : hmonien@uni-bonn.de  
+-}
+
+module Data.Number.Flint.Groups.Perm (
+  module Data.Number.Flint.Groups.Perm.FFI,
+) where
+
+import Data.Number.Flint.Groups.Perm.FFI
+
