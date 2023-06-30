9ad7cdf54e2156187799f0014cc3e019ee83d9b5
Hartmut Monien <hmonien@uni-bonn.de>
2023-06-30 22:48:21 +0200

    added documentation to UFD.

--- src/Data/Number/Flint/UFD.hs
+++ src/Data/Number/Flint/UFD.hs
@@ -1,6 +1,18 @@
+{-| 
+Module: Data.Number.Flint.UFD 
+
+= Unique factorization domain
+
+Specifically, a UFD is an integral domain (a nontrivial commutative ring in which the product of any two non-zero elements is non-zero) in which every non-zero non-unit element can be written as a product of prime elements (or irreducible elements), uniquely up to order and units.
+-}
 module Data.Number.Flint.UFD where
 
 class (Num a) => UFD a where
+  -- | factor /x/ 
+  --
+  -- factor /x/ into `prime` factors \(x = p_1^{e_1}\ldots p_n^{e_n}\) 
+  -- with the representation \([(p_1, e_1) \ldots (p_n, e_n)]\)
+  --
   factor :: a -> [(a, Int)]
   unfactor :: [(a, Int)] -> a
   unfactor x = product $ map (uncurry (^)) x
